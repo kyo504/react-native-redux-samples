5c77ac703c6d44fbe3e9c91c87d1eb21eeb61940
Hunkyo Jung <hunkyo.jung@lge.com>
2016-07-26 14:59:14 +0900

    Destruct props

--- Counter/Counter.js
+++ Counter/Counter.js
@@ -21,16 +21,19 @@ export default class Counter extends Component {
   }
 
   render() {
+
+    const { onIncrement, onDecrement } = this.props;
+
     return (
       <View style={styles.container}>
         <Text style={{fontSize:20, fontWeight:'bold'}}>{this.props.value}</Text>
         <View style={{flexDirection:'row'}}>
-          <TouchableHighlight onPress={() => this.props.onIncrement()}>
+          <TouchableHighlight onPress={() => onIncrement()}>
             <View style={{width:20, borderWidth:1, borderColor:'black'}}>
               <Text style={{textAlign:'center'}}>+</Text>
             </View>
           </TouchableHighlight>
-          <TouchableHighlight onPress={() => this.props.onDecrement()}>
+          <TouchableHighlight onPress={() => onDecrement()}>
             <View style={{width:20, borderWidth:1, borderColor:'black'}}>
               <Text style={{textAlign:'center'}}>-</Text>
             </View>
