1aa3f22ecb27bf05440fa326eceed49babf55528
Hun Kyo Jung <hunkyo.jung@gmail.com>
2017-04-14 15:39:36 +0900

    Change import statement

--- TodoListRedux/index.ios.js
+++ TodoListRedux/index.ios.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { AppRegistry } from 'react-native';
 import { Provider } from 'react-redux';
 import { applyMiddleware, createStore } from 'redux';
-import createLogger from 'redux-logger';
+import { createLogger } from 'redux-logger';
 
 import App from './app/App'
 import reducers from './app/reducers'
