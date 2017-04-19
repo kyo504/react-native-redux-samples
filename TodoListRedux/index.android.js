ca65d84c607dda7a8aec6badc4d2270f4b5d1f26
Hun Kyo Jung <hunkyo.jung@gmail.com>
2017-04-19 15:22:11 +0900

    Passing the store down with Provider from react-redux

--- TodoListRedux/index.android.js
+++ TodoListRedux/index.android.js
@@ -1,34 +1,9 @@
 import React from 'react';
 import { AppRegistry } from 'react-native';
-//import { Provider } from 'react-redux';
+import { Provider } from 'react-redux';
 
 import TodoApp from './app/App';
 import store from './app/store/configureStore';
-
-/*const TodoList = () => {
-  return (
-    <Provider store={store}>
-      <App />
-    </Provider>
-  );
-}*/
-
-
-class Provider extends Component {
-  getChildContext() {
-    return {
-      store: this.props.store,
-    }
-  }
-
-  render() {
-    return this.props.children;
-  }
-}
-
-Provider.childContextTypes = {
-  store: React.PropTypes.object,
-}
 
 const App = () => {
   return (
